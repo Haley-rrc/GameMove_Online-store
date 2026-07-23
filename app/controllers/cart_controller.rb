7deb8a374cf40463caf20a3ffaec29c68cf375b0
HALEY.J <hjiang35@rrc.ca>
2026-07-23 03:25:08 -0500

    Add cart quantity update and item removal

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,58 @@
 class CartController < ApplicationController
-  # Show all items currently saved in the cart.
+  # Show all items saved in the shopping cart.
   def show
+    load_cart_items
+  end
+
+  # Add one product to the cart.
+  def add
+    product = Product.find(params[:product_id])
+
+    session[:cart] ||= {}
+
+    product_id = product.id.to_s
+    current_quantity = session[:cart][product_id].to_i
+
+    session[:cart][product_id] = current_quantity + 1
+
+    flash[:notice] = "#{product.name} was added to your cart."
+    redirect_to cart_path
+  end
+
+  # Update the quantity of one cart item.
+  def update
+    product = Product.find(params[:product_id])
+    quantity = params[:quantity].to_i
+
+    session[:cart] ||= {}
+
+    if quantity < 1
+      flash[:alert] = "Quantity must be at least 1."
+    elsif quantity > product.stock_quantity
+      flash[:alert] = "Not enough stock is available."
+    else
+      session[:cart][product.id.to_s] = quantity
+      flash[:notice] = "#{product.name} quantity was updated."
+    end
+
+    redirect_to cart_path
+  end
+
+  # Remove one product from the cart.
+  def remove
+    product = Product.find(params[:product_id])
+
+    session[:cart] ||= {}
+    session[:cart].delete(product.id.to_s)
+
+    flash[:notice] = "#{product.name} was removed from your cart."
+    redirect_to cart_path
+  end
+
+  private
+
+  # Build cart item data for the cart page.
+  def load_cart_items
     cart = session[:cart] || {}
 
     @cart_items = cart.map do |product_id, quantity|
@@ -16,20 +68,5 @@ class CartController < ApplicationController
     end.compact
 
     @cart_total = @cart_items.sum { |item| item[:item_total] }
-  end
-
-  # Add one product to the session cart.
-  def add
-    product = Product.find(params[:product_id])
-
-    session[:cart] ||= {}
-
-    product_id = product.id.to_s
-    current_quantity = session[:cart][product_id].to_i
-
-    session[:cart][product_id] = current_quantity + 1
-
-    flash[:notice] = "#{product.name} was added to your cart."
-    redirect_to cart_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
   get "cart", to: "cart#show", as: :cart
   post "cart/add/:product_id", to: "cart#add", as: :add_to_cart
 
+  patch "cart/update/:product_id",
+        to: "cart#update",
+        as: :update_cart_item
+
+  delete "cart/remove/:product_id",
+        to: "cart#remove",
+        as: :remove_cart_item
+
   namespace :admin do
     get "login", to: "sessions#new"
     post "login", to: "sessions#create"
